861dab2c0928aa55f96ae9b2b677b488fd51ccfe
Nurech <joosep.parts@gmail.com>
2024-11-30 21:59:56 +0200

    local

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService} from '@angular/fire/analytics';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getFunctions, provideFunctions} from '@angular/fire/functions';
+import {getMessaging, provideMessaging} from '@angular/fire/messaging';
+import {getPerformance, providePerformance} from '@angular/fire/performance';
+import {getStorage, provideStorage} from '@angular/fire/storage';
+import {getRemoteConfig, provideRemoteConfig} from '@angular/fire/remote-config';
+import { provideStore } from '@ngrx/store';
+import {environment} from '../enviorments/enviorment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStore()
+],
 };
