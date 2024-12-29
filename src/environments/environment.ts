9f8828450a480a47a354cad283dbc3f847da1173
Nurech <joosep.parts@gmail.com>
2024-12-29 08:18:23 +0200

    gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     projectId: 'kaleviok-9aeb0',
     appId: '1:287075974601:web:b6cb1732b3c3fc30567c18',
     storageBucket: 'kaleviok-9aeb0.firebasestorage.app',
-    apiKey: 'DEV_API_KEY',
+    apiKey: 'DUMMY_API_KEY',
     authDomain: 'kaleviok-9aeb0.firebaseapp.com',
     messagingSenderId: '287075974601',
     measurementId: 'G-DV660QJZQN',
