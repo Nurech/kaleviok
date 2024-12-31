8bc95a299eacf30ab33bd68c82a2807d5444079c
Nurech <joosep.parts@gmail.com>
2024-12-31 15:31:52 +0200

    test workflows

--- src/environments/environment.template.ts
+++ src/environments/environment.template.ts
@@ -8,6 +8,5 @@ export const environment = {
     authDomain: '$FIREBASE_AUTH_DOMAIN',
     messagingSenderId: '$FIREBASE_MESSAGING_SENDER_ID',
     measurementId: '$FIREBASE_MEASUREMENT_ID',
-    appName: '$FIREBASE_APP_NAME',
   },
 };
