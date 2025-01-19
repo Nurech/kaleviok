0bbcb86e9ac5fd5a62b803185dd66409a231f5a5
Nurech <joosep.parts@gmail.com>
2025-01-19 18:06:00 +0200

    crumbs selector

--- src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,10 +1,10 @@
-import { Component, Input, Signal, computed, inject } from '@angular/core';
+import { Component, Signal, inject } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { NgForOf, NgIf } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-import { selectRouter } from '../../../store/router/router.selectors';
+import { selectBreadcrumbs } from '../../../store/router/router.selectors';
 
 @Component({
     selector: 'app-breadcrumbs',
@@ -14,33 +14,11 @@ import { selectRouter } from '../../../store/router/router.selectors';
     imports: [NgForOf, MatIcon, NgIf, RouterLink]
 })
 export class BreadcrumbsComponent {
-    @Input() maxCrumbs = 5; // Set a max limit for breadcrumbs
-    store$ = inject(Store);
-
-    private routerState = toSignal(this.store$.pipe(select(selectRouter)), { initialValue: null });
-    breadcrumbs: Signal<{ label: string; url: string }[]> = computed(() => {
-        const state = this.routerState()?.state;
-        if (!state) return [];
-
-        const segments = state.url.split('/').filter((segment: any) => segment);
-        let fullPath = '';
-        const crumbs = segments.map((segment: any) => {
-            fullPath += `/${segment}`;
-            return { label: segment, url: fullPath };
-        });
-
-        return this.formatCrumbs(crumbs);
-    });
-
-    private formatCrumbs(crumbs: { label: string; url: string }[]): { label: string; url: string }[] {
-        if (crumbs.length <= this.maxCrumbs) return crumbs;
-
-        const first = crumbs[0];
-
-        return [
-            first,
-            { label: '...', url: '' },
-            ...crumbs.slice(-this.maxCrumbs + 2) // Keep last few items
-        ];
-    }
+    private store$ = inject(Store);
+    breadcrumbs: Signal<
+        {
+            label: string;
+            url: string;
+        }[]
+    > = toSignal(this.store$.pipe(select(selectBreadcrumbs)), { initialValue: [] });
 }
--- src/app/store/router/router.selectors.ts
+++ src/app/store/router/router.selectors.ts
@@ -18,3 +18,27 @@ export const {
 
 export const selectEditMode = createSelector(selectQueryParams, (queryParams) => queryParams['edit'] === 'true');
 export const selectAccountId = createSelector(selectQueryParams, (queryParams) => queryParams['id'] || null);
+
+export const selectBreadcrumbs = createSelector(selectRouter, (routerState) => {
+    const state = routerState?.state?.root;
+    if (!state) return [];
+
+    const segments = [];
+    let currentNode = state.firstChild;
+    let fullPath = '';
+
+    while (currentNode) {
+        const pathSegment = currentNode.url.map((segment) => segment.path).join('/');
+        if (pathSegment) {
+            fullPath += `/${pathSegment}`;
+            segments.push({ label: pathSegment, url: fullPath });
+        }
+        currentNode = currentNode.children.length ? currentNode.children[0] : null;
+    }
+
+    if (segments.length > 5) {
+        return [{ label: '...', url: '' }, ...segments.slice(-4)];
+    }
+
+    return segments;
+});
