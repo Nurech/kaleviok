65f193e100cfc394f351d3b4f577f839ffc4135a
Nurech <joosep.parts@gmail.com>
2025-01-20 05:29:05 +0200

    fix sidenav animations

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -1,10 +1,9 @@
 import { NgComponentOutlet } from '@angular/common';
-import { Component, computed, inject, output, signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
-import { Store } from '@ngrx/store';
 import { DarkModeSwitchComponent } from '../../../shared/components/dark-mode-switch/dark-mode-switch.component';
 import { MenuService } from '../../../shared/services/menu.service';
 
@@ -30,9 +29,7 @@ import { MenuService } from '../../../shared/services/menu.service';
     ]
 })
 export class SidenavComponent {
-    store$ = inject(Store);
-    closeDrawer = output();
-    menuItems = inject(MenuService).rootMenuItems;
-    public isOpen = signal(false);
-    animationState = computed(() => (this.isOpen() ? 'open' : ''));
+    private menuService = inject(MenuService);
+    menuItems = this.menuService.rootMenuItems;
+    animationState = computed(() => (this.menuService.isDrawerOpen() ? 'open' : ''));
 }
